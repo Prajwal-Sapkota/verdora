36cef9305aace7652e4e0c59d3ba12378c98b589
Prajwal-Sapkota <prajwalsapkota497@gmail.com>
2026-01-26 12:09:10 +0545

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { visualizer } from 'rollup-plugin-visualizer'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), 
-    tailwindcss(),
-    visualizer({
-      open: true,
-      filename: 'dist/stats.html',
-    })
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Force ALL node_modules into separate chunks
-          if (id.includes('node_modules')) {
-            if (id.includes('react-icons')) return 'vendor-icons';
-            if (id.includes('react')) return 'vendor-react';
-            return 'vendor';
-          }
-        }
-      }
-    }
+  plugins: [react(), tailwindcss(),],
+   build: {
+    sourcemap: true
   }
 })
